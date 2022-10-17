475f5ac966aeec5c7cd8e34d5f21ae673a7ae85e
Igors Šamrovs <114918668+isamrovs@users.noreply.github.com>
2022-10-17 11:03:49 +0000

    2. uzdevums

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from math import*
-sk1=int(input("Ievadiet trijstūra vislielāko leņķi: "))
-sk2=int(input("Ievadiet otro trijstūra leņķi: "))
-sk3=int(input("Ievadiet trešo trijstūra leņķi: "))
-if sk1+sk2+sk3==180:
- if sk1==90:
-   print("Tas ir taisnleņķa trijstūris!")
- elif sk1>90:
-   print("Tas ir platleņķa trijstūris!")
- elif sk1<90:
-   print("Tas ir šaurleņķu trijstūris!")
+sk1=int(input("Ievadiet mēneša numuru: "))
+if sk1<13:
+  if sk1==1:
+   print("Tas is janvāris!")
+  elif sk1==2:
+    print("Tas is februāris!")
+  elif sk1==3:
+    print("Tas is marts!")
+  elif sk1==4:
+    print("Tas is aprīlis!")
+  elif sk1==5:
+    print("Tas is maijs!")
+  elif sk1==6:
+    print("Tas is jūnijs!")
+  elif sk1==7:
+    print("Tas is jūlijs!")
+  elif sk1==8:
+    print("Tas is augusts!")
+  elif sk1==9:
+    print("Tas is septembris!")
+  elif sk1==10:
+    print("Tas is oktobris!")
+  elif sk1==11:
+    print("Tas is novembris!")
+  elif sk1==12:
+    print("Tas is decembris!")
 else:
-  print("Tas nav trijstūris!")
+  print("Šāda mēneša nav!")
