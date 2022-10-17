7319a531d1c61aa50820b1eb03a7ccf5135f4a3d
Igors Šamrovs <114918668+isamrovs@users.noreply.github.com>
2022-10-17 11:46:05 +0000

    4. uzdevums

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from math import*
-x=int(input("Ievadiet x: "))
-y=int(input("Ievadiet y: "))
-if x>0:
- if y>0:
-  print("Punkts atrodas pirmajā kvadrantā!")
-if x>0:
- if y<0:
-  print("Punkts atrodas ceturtajā kvadrantā!")
-if x<0:
- if y<0:
-  print("Punkts atrodas trešajā kvadrantā!")
-if x<0:
- if y>0:
-  print("Punkts atrodas otrajā kvadrantā!")
+sk1=int(input("Ievadiet ciparu: "))
+if sk1>0:
+  if sk1<10:
+    print("Tas ir vienciparu skaitlis!")
+if sk1<0:
+  if sk1>-10:
+    print("Tas ir vienciparu skaitlis!")
+if sk1==0:
+  print("Tas ir vienciparu skaitlis!")
+if sk1>0:
+  if sk1>9:
+    if sk1<100:
+      print("Tas ir divciparu skaitlis!")
+if sk1<0:
+ if sk1<-9:
+  if sk1>-100:
+   print("Tas ir divciparu skaitlis!")
+if sk1>0:
+  if sk1>99:
+   if sk1<1000:
+     print("Tas ir trīsciparu skaitlis!")
+if sk1<0:
+  if sk1<-99:
+   if sk1>-1000:
+     print("Tas ir trīsciparu skaitlis!")
+if sk1>0:
+  if sk1>999:
+    print("Tas ir vairāk ciparu skaitlis!")
+if sk1<0:
+  if sk1<-999:
+    print("Tas ir vairāk ciparu skaitlis!")
+if sk1>0:
+ print("Skaitlis ir pozitīvs!")
+if sk1<0:
+  print("Skaitlis ir negatīvs!")
+if sk1==0:
+  print("Tas nav pozitīvs un nav negatīvs skaitlis!")
